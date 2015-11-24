453ee61b7014b8d819c49d5c3cfd1c0c94144402
Jovanka Helen Maradenia <jovankalubis@gmail.com>
2015-11-24 12:05:50 +0700

    Update UlarTangga.py

--- UlarTangga.py
+++ UlarTangga.py
@@ -26,3 +26,44 @@ class Dice:
 	def getMataDadu(self):
                 self.mataDadu = random.randint(1,6)
                 return self.mataDadu
+            
+class Tile:
+	player = ''
+	ladderAwal = ''
+	snakeAwal = ''
+	def getPlayer(self):
+		return self.player
+	def setPlayer(self, player):
+		self.player = player
+	def getLadderAwal(self):
+		return self.ladderAwal
+	def setLadderAwal(self, ladderAwal):
+		self.ladderAwal = ladderAwal
+	def getSnakeAwal(self):
+		return self.snakeAwal
+	def setSnakeAwal(self, snakeAwal):
+		self.snakeAwal = snakeAwal
+
+class Board:
+	tile = [""]
+	posisiPlayer = 0
+	player = ""
+	mataDadu = 0
+	def __init__(self):
+            self.tile = range(1,101)
+            self.posisiPlayer = 0
+	def setPlayer(self,player):
+            self.player = player
+	def setPosition(self,x):
+            self.posisiPlayer = self.player.getPosition() + x
+            if(self.posisiPlayer > 100):
+                m = self.posisiPlayer - 100
+                self.posisiPlayer = 100 - m
+            self.player.setPosition(self.posisiPlayer)
+	def setMataDadu(self,dice):
+		self.mataDadu = dice.getMataDadu()
+	def getMataDadu(self):
+		return self.mataDadu
+	def getPlayer():
+		return self.player
+           
